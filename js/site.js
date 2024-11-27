baef988ac07928ad71adbd5e15bc3ff049da7109
Suleman Jaffri <122302601+Suljaf12@users.noreply.github.com>
2024-11-28 02:37:44 +0500

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,19 @@
+const header = document.querySelector("header");
+const images = [
+  "Media/Header1.jpg",
+  "Media/Header2.jpg",
+  "Media/Header3.jpg"
+];
+
+let currentImageIndex = 0;
+
+document.getElementById("change-image-btn").addEventListener("click", function () {
+
+  header.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+  header.style.backgroundSize = "cover";
+  header.style.backgroundPosition = "center";
+  header.style.backgroundRepeat = "no-repeat";
+  header.style.height = "3rem";
+
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+});
